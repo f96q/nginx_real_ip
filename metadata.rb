34a02efbb3d539c851d9a6db4a2736bc74694160
danny <danny.dev8@gmail.com>
2015-06-25 21:27:05 +0900

    remove depends

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,3 @@ license          'MIT'
 description      'Installs/Configures nginx_real_ip'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-depends          'nginx'
